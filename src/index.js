10c1aaa6c1cde2a3a57c4766187951b53ee0db4f
Nguyễn Quang Duy <nqduy98999@gmail.com>
2022-03-14 23:14:06 +0700

    Refacor code: create routes, app controllers folders

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -23,23 +25,7 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-    res.render('home')
-});
-
-app.get('/news', (req, res) => {
-    console.log(req.query.q)
-    res.render('news')
-});
-
-app.get('/search', (req, res) => {
-    res.render('search')
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body.q)
-    res.send('')
-});
+// Route init
+route(app);
 
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`))
